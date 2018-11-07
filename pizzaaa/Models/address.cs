b759b8464c0fd74919e1d7a165a6fc7cdd40599c
Jerminayman <44840402+Jerminayman@users.noreply.github.com>
2018-11-07 16:36:25 +0200

    gnf

---
+++ pizzaaa/Models/address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pizzaaa.Models
+{
+    public class address
+    {
+        public string street;
+        public string department;
+    }
+}
